20e43d297e6910d6e25dbff9cdbf5d4246feb6e6
razmoose <s.moosaraza.97@gmail.com>
2022-06-29 16:00:21 +0500

    component creation + nesting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {ServerComponent} from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningComponent } from './warning/warning.component';
+import { SuccessComponent } from './success/success.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    WarningComponent,
+    SuccessComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    FormsModule,
+    AppRoutingModule  ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/success/success.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-success',
+    templateUrl: './success.component.html',
+    styles: [`
+    h2{color:green;
+    background-color:grey;}`]
+})
+
+export class SuccessComponent{}
