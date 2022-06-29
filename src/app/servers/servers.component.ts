722a027aa9b2ebcd83dd937384311cdf5f522753
razmoose <s.moosaraza.97@gmail.com>
2022-06-29 18:05:06 +0500

    data binding concepts
    name as input and a reset button

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,14 +2,30 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: '[app-servers]',
-  template: '<app-server></app-server><app-server></app-server>',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowReset = false;
+  serverCreationStatus = 'No server created';
+  username = '';
+  constructor() { 
+  }
 
   ngOnInit(): void {
   }
+
+
+
+  onUserInput(){
+    this.allowReset=true;
+  }
+
+  resetUser(){
+    this.username= '';
+    this.allowReset=false;
+  }
+
 
 }
